10e5f5b3bf9d0794a70fff97ef5dce66ac6c8cda
harbeyme24 <143273418+harbeyme24@users.noreply.github.com>
2023-12-08 19:56:55 +0800

    Add files via upload

--- NewsSubscriptionService.java
+++ NewsSubscriptionService.java
@@ -8,51 +8,51 @@ public class NewsSubscriptionService {
 
         NewsAgency newsAgency = new NewsAgency();
 
-        System.out.println("Welcome to 24 Oras News Agency!\n");
+        System.out.println("Welcome to NET 25 Agency!\n");
 
         //Creating Objects
 
-        Subscriber noriel = new Subscriber();
-        noriel.setSubscriberName("Noriel");
+        Subscriber harbey = new Subscriber();
+        noriel.setSubscriberName("Harbey");
 
-        Subscriber leiron = new Subscriber();
-        leiron.setSubscriberName("leiron");
+        Subscriber manuel = new Subscriber();
+        leiron.setSubscriberName("Manuel");
 
-        Subscriber achero = new Subscriber();
-        achero.setSubscriberName("achero");
+        Subscriber edroso = new Subscriber();
+        achero.setSubscriberName("Edroso");
 
-        Subscriber orecha = new Subscriber();
-        orecha.setSubscriberName("orecha");
+        Subscriber ladignon = new Subscriber();
+        orecha.setSubscriberName("Ladignon");
 
         //Subscribing Objects
 
-        newsAgency.subscribe(noriel);
-        newsAgency.subscribe(leiron);
-        newsAgency.subscribe(achero);
-        newsAgency.subscribe(orecha);
+        newsAgency.subscribe(harbey);
+        newsAgency.subscribe(manuel);
+        newsAgency.subscribe(edroso);
+        newsAgency.subscribe(ladignon);
 
         //Notifying the Subscribers
 
         System.out.println("Here are news for these subscribers!\n");
-        newsAgency.notifySubscribers("Kathniel broke up", "Celebrity");//Flexibility for adding updated news and its category
+        newsAgency.notifySubscribers("E.A.T. Bulaga", "Celebrity");//Flexibility for adding updated news and its category
 
 
         //Unsubscribing Objects
-        newsAgency.unsubcribe(noriel);
-        newsAgency.unsubcribe(achero);
+        newsAgency.unsubcribe(harbey);
+        newsAgency.unsubcribe(edroso);
 
         //Notifying the Subscribers
 
         System.out.println("\nHere are news for these subscribers!\n");
-        newsAgency.notifySubscribers("Lumindol kaninang hapon!", "Calamity");//Flexibility for adding updated news and its category
+        newsAgency.notifySubscribers("Lumindol yumanig nakakatakot!", "Calamity");//Flexibility for adding updated news and its category
 
-        Subscriber John = new Subscriber();
-        John.setSubscriberName("John");
+        Subscriber Sofia = new Subscriber();
+        John.setSubscriberName("Sofia");
 
-        newsAgency.subscribe(John);
+        newsAgency.subscribe(Sofia);
 
         System.out.println("\nHere are news for these subscribers!\n");
-        newsAgency.notifySubscribers("Iphone releases Iphone 16", "Technology");//Flexibility for adding updated news and its category
+        newsAgency.notifySubscribers("Apple releases IPad Pro M3", "Technology");//Flexibility for adding updated news and its category
 
 
 
